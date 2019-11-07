d27332f78c6dafa5ea1c08be5b9171e6a41f46b7
Macarena Poo <macarenapoo@gmail.com>
2019-11-06 22:01:09 -0600

    Examen 2

---
+++ testimoniales.php
@@ -0,0 +1,12 @@
+<?php
+  include "../conexion.php";
+  $sql = "select * from testimoniales";
+  $rs = ejecutar($sql);
+?>
+
+<script>
+  var testimoniales = new Array();
+  <?php
+    // agrega aqui tu código
+  ?>
+</script>
